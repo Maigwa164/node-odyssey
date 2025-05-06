aae36a79e3f91627d01423f700e7f135df56d1cd
David Maigwa <davidmaigwa164@gmail.com>
2025-05-06 23:52:04 +0300

    reading on a file

--- first-app.js
+++ first-app.js
@@ -2,3 +2,6 @@ console.log("Hello from Node.js");
 
 const fs = require("fs");
 fs.writeFileSync("hello.txt", "Hello David I am writing on your File");
+
+const content = fs.readFileSync("hello.txt", "utf-8");
+console.log(content);
