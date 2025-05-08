173d0627b3314c108d807c4c698d75c971bfb62e
David Maigwa <davidmaigwa164@gmail.com>
2025-05-08 12:38:41 +0300

    Understanding requests(url,method,headers)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method, req.headers);
+
+  // process.exit();---->should not be avoided as it hard shutthe server
 });
 
 server.listen(3000);
