2bcbc0b46900458a714ebd3f8c08fc6df9945ad8
David Maigwa <davidmaigwa164@gmail.com>
2025-05-22 11:37:36 +0300

    creating submission form

--- assignment/app.js
+++ assignment/app.js
@@ -2,11 +2,20 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   // console.log(req);
   const url = req.url;
+  const method = req.method;
 
   if (url === '/') {
+    // res.write('<html>');
+    // res.write('<head><title>some greetings</title></head>');
+    // res.write('<body><h2>Heey David Again<h2></body');
+    // res.write('</html>');
+    // return res.end();
+
     res.write('<html>');
-    res.write('<head><title>some greetings</title></head>');
-    res.write('<body><h2>Heey David Again<h2></body');
+    res.write('<head><title>form</title></head>');
+    res.write(
+      '<body><form action = "./create-user" method = "POST"><input type="text" name="user"></input><button type ="submit">submit</button></form></body'
+    );
     res.write('</html>');
     return res.end();
   } else if (url === '/users') {
@@ -17,6 +26,9 @@ const server = http.createServer((req, res) => {
     );
     res.write('</html>');
     return res.end();
+  }
+  if (url === './create-user' && method === 'POST') {
+    console.log(developing);
   }
 });
 server.listen(8080);
