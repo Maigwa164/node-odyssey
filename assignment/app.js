17fc0c1bf0c047fc2129a40a9e095c4c2809ac68
David Maigwa <davidmaigwa164@gmail.com>
2025-05-22 11:24:26 +0300

    routes

--- assignment/app.js
+++ assignment/app.js
@@ -4,6 +4,19 @@ const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>some greetings</title></head>');
+    res.write('<body><h2>Heey David Again<h2></body');
+    res.write('</html>');
+    return res.end();
+  } else if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><title>some greetings</title></head>');
+    res.write(
+      '<body><ul><li>David Maigwa</li> <li>Abel Nyaga</li> <li>Winnie Maigwa</li> <li>Margaret Maigwa</li></ul><h2></body'
+    );
+    res.write('</html>');
+    return res.end();
   }
 });
 server.listen(8080);
