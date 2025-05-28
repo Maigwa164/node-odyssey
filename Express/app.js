da33aee20576c66bafe5bb8991f8a74ce9d54ba5
David Maigwa <davidmaigwa164@gmail.com>
2025-05-28 16:14:20 +0300

    Parsing incoming request

--- Express/app.js
+++ Express/app.js
@@ -3,19 +3,28 @@
 
 // ****npm modules***
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+// app.use('/', (req, res, next) => {
+//   console.log('This always runs');
+//   next();
+// });
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>The "Add Product" Page');
+  res.send(
+    '<form action = "/product" method = "POST"><input = "text" name = "title"></input><button type = "submit">submit</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In the second middleware');
+  // console.log('In the second middleware');
   res.send('<h1>Hello from Express</h1>');
 });
 
