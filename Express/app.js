b57c6675d48f205291dcf60926050e1b393df2e7
David Maigwa <davidmaigwa164@gmail.com>
2025-05-29 00:15:38 +0300

    Using Express Router

--- Express/app.js
+++ Express/app.js
@@ -6,27 +6,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// app.use('/', (req, res, next) => {
-//   console.log('This always runs');
-//   next();
-// });
+//*****my modules */
+const adminRoute = require('./routes/admin.js');
+const shopRoute = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded());
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action = "/product" method = "POST"><input = "text" name = "title"></input><button type = "submit">submit</button></form>'
-  );
-});
+app.use(adminRoute);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  // console.log('In the second middleware');
-  res.send('<h1>Hello from Express</h1>');
-});
+app.use(shopRoute);
 
 // const server = http.createServer(app);
 // server.listen(3000);****shortend to ⏓
