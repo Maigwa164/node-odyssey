38335f9765a025b56efda92b3294c08fcb9c3b00
David Maigwa <davidmaigwa164@gmail.com>
2025-05-28 13:02:42 +0300

    Build an express app that handles two requests

--- assignment_express/app.js
+++ assignment_express/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {});
+app.use('/user', (req, res, next) => {
+  res.send('<ul><li>David</li> <li>Muiruri</li> <li>Maigwa</li></ul>');
+});
 
-app.use((req, res, next) => {});
+app.use('/', (req, res, next) => {
+  res.send('<h1>Welcome to the home page</h1>');
+});
 
 app.listen(3000);
