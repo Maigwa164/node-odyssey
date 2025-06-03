b9888aae28fe585445cfd6d31e6b335a80eb9066
David Maigwa <davidmaigwa164@gmail.com>
2025-06-03 14:38:14 +0300

    Filtering paths

--- Express/app.js
+++ Express/app.js
@@ -12,9 +12,13 @@ const shopRoute = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded());
 
-app.use(adminRoute);
+app.use('/admin', adminRoute);
 
 app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1><i>404 Page not found</i></h1>');
+});
 
 // const server = http.createServer(app);
 // server.listen(3000);****shortend to ⏓
