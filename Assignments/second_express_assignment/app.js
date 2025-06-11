4565dc0b9ede3521b09b21a1fc235b0755e88002
David Maigwa <davidmaigwa164@gmail.com>
2025-06-12 00:09:56 +0300

    finished the express assignment creating 3 routes

--- Assignments/second_express_assignment/app.js
+++ Assignments/second_express_assignment/app.js
@@ -3,19 +3,26 @@ const path = require('path');
 
 //package modules
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //My packages
 const homeRoute = require('./routes/home.js');
 const userRoutes = require('./routes/user.js');
+const addUserRoute = require('./routes/add-user.js');
 
 //-------****--------
 const app = express();
 
+//Converting parsed body content
+app.use(bodyParser.urlencoded());
+
 // middlewares routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+//middleware funnel
 app.use(homeRoute);
 app.use(userRoutes);
+app.use(addUserRoute);
 
 app.use('/', (req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
