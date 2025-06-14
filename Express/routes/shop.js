736e917ba95aa6a8ebcf856686243b6cba157e32
David Maigwa <davidmaigwa164@gmail.com>
2025-06-14 16:42:20 +0300

    Making a pug file for the shop view

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -9,8 +9,9 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   // console.log('In the second middleware');
   // res.send('<h1>Hello from Express</h1>');
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop'); //rendering shop dynamically
 });
 
 module.exports = router;
