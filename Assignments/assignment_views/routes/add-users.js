20f49ccab48bd5ade2a713dcfcf79fd9fa689b38
David Maigwa <davidmaigwa164@gmail.com>
2025-06-27 22:12:34 +0300

    Finished serving files and CSS

--- Assignments/assignment_views/routes/add-users.js
+++ Assignments/assignment_views/routes/add-users.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-user', (req, res, next) => {
+router.get('/add-users', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'add-users.html'));
 });
 
