701b9b826349513aaf34cdd0f443ea5e754cd039
David Maigwa <davidmaigwa164@gmail.com>
2025-06-28 15:13:42 +0300

    Finished rendering POST request for users List

--- Assignments/assignment_views/routes/users.js
+++ Assignments/assignment_views/routes/users.js
@@ -11,8 +11,8 @@ const addUserData = require('./add-users');
 
 router.get('/', (req, res, next) => {
   // res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
-  console.log(addUserData.users);
-  res.render('users', { docTitle: 'users' });
+  const usersList = addUserData.users;
+  res.render('users', { docTitle: 'users', users: usersList });
 });
 
 module.exports = router;
